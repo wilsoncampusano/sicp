adc5bdfd386c69cb7a9cd396a8da2f101a76ef35
Wilson Campusano <wilsoncampusanojorge@gmail.com>
2018-04-06 16:12:34 -0400

    format

---
+++ scheme/square_roots.scm
@@ -0,0 +1,28 @@
+(define (sqrt-iter guess x)
+  (if (good-enough? guess x )
+      guess
+      (sqrt-iter (improve guess x) x)
+  )
+)
+
+(define (good-enough? guess x )
+  (< (abs (- (square guess ) x)) 0.0001)
+)
+
+(define (improve guess x )
+  ( average guess (/ x guess) )
+)
+
+(define (average x y )
+  ( / (+ x y ) 2 )
+)
+
+(define (sqrt x )
+  (sqrt-iter 1.0 x )
+)
+
+(sqrt 9)
+
+(sqrt (+ 100 37))
+
+(sqrt 25)
