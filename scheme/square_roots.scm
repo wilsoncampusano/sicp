7f07bee021c092016150388ab454697099609f42
Wilson Campusano <wilsoncampusanojorge@gmail.com>
2018-04-07 09:28:28 -0400

    lexical scoping

--- scheme/square_roots.scm
+++ scheme/square_roots.scm
@@ -1,24 +1,25 @@
-(define (sqrt-iter guess x)
-  (if (good-enough? guess x )
-      guess
-      (sqrt-iter (improve guess x) x)
-  )
-)
-
-(define (good-enough? guess x )
-  (< (abs (- (square guess ) x)) 0.0001)
-)
-
-(define (improve guess x )
-  ( average guess (/ x guess) )
-)
-
-(define (average x y )
-  ( / (+ x y ) 2 )
-)
-
 (define (sqrt x )
-  (sqrt-iter 1.0 x )
+
+  (define (sqrt-iter guess)
+    (if (good-enough? guess)
+        guess
+        (sqrt-iter (improve guess))
+    )
+  )
+
+  (define (good-enough? guess)
+    (< (abs (- (square guess ) x )) 0.0001)
+  )
+
+  (define (improve guess)
+    (average guess (/ x guess ))
+  )
+
+  (define (average x y)
+    (/ (+ x y ) 2)
+  )
+
+  (sqrt-iter 1.0   )
 )
 
 (sqrt 9)
